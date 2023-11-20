1a48ee936c4cf6b765aa4ad8c82335231c462375
pauline-busch <73955451+pauline-busch@users.noreply.github.com>
2023-11-20 15:39:55 +0100

    additions to repository
    added code for p-value bars
    added result figure

--- boxplot_repletion.R
+++ boxplot_repletion.R
@@ -5,14 +5,14 @@ library(data.table)
 
 
 all_Data <- data.table(
-  read.xlsx("result_repletion.xlsx"))
+  read.xlsx("data_tables/Microscopy_repletion.xlsx"))
 
 type = factor(all_Data$CellType, level = c("Discocyte", "Stomatocyte", "kA"))
 
 ggplot(data = all_Data,
        aes(x = factor(
          sample, 
-         level = c("control", "CDNB", "DTT", "etGSH")), 
+         level = c("control", "CDNB60", "DTT15", "etGSH30")), 
            y = CellPercent)) +
   
   geom_boxplot(show.legend = FALSE, aes(fill = type)) +
@@ -35,14 +35,14 @@ ggplot(data = all_Data,
   
   theme_light() +
   
-  stat_compare_means(comparisons = list(c("control","CDNB"),
-                                        c("control","DTT"),
-                                        c("control","etGSH")),
-                                        # c("CDNB","DTT"),
-                                        # c("CDNB","etGSH"),
-                                        # c("DTT","etGSH")),
+  stat_compare_means(comparisons = list(c("control","CDNB60"),
+                                        c("control","DTT15"),
+                                        c("control","etGSH30"),
+                                        c("CDNB60","DTT15"),
+                                        c("CDNB60","etGSH30"),
+                                        c("DTT15","etGSH30")),
                      label = "p.signif", method = "wilcox.test")
 
 
-ggsave("result_repletion.tiff", path = "C:/Users/miri/OneDrive/Desktop/R/", units = "in", dpi=300, compression = 'lzw', width = 8, height = 6)
+ggsave("result_repletion.tiff", path = "C:/Users/Rickman/Documents/GitHub/R-graphs/Graphs and figures", units = "in", dpi=300, compression = 'lzw', width = 8, height = 6)
 
