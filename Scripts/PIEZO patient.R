0b0f45110dc60cb93f8ff6973bbd1ee2a5ff88bd
pauline-busch <miri@mccon.de>
2024-02-21 20:52:04 +0100

    development of code
    PIEZO patient graphs for weekly meeting

--- Scripts/PIEZO patient.R
+++ Scripts/PIEZO patient.R
@@ -7,6 +7,17 @@ library(dplyr)
 
 results <- read.xlsx("PIEZO-patient.xlsx")
 
-GSH.results <- results
+GSH.results <- results %>% filter(experiment == "ATP")
+ATP.results <- results %>% filter(experiment == "GSH")
 
-ggplot(results, )
+GSH.graph <- ggplot(GSH.results, aes(sample, value, fill = sample)) + 
+  
+  geom_col() +
+  scale_fill_manual(values = c("#b00000", "#b19999"))
+
+ATP.graph <- ggplot(ATP.results, aes(sample, value, fill = sample)) + 
+  
+  geom_col() +
+  scale_fill_manual(values = c("#b00000", "#b19999"))
+
+print(ATP.graph)
