d4225839daccfcab41cce8045baafc424479c132
pauline-busch <miri@mccon.de>
2024-02-21 20:16:40 +0100

    new analysis file

---
+++ Scripts/PIEZO patient.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+library(ggpubr)
+library(ggthemes)
+library(openxlsx)
+library(data.table)
+library(dplyr)
+
+results <- read.xlsx("PIEZO-patient.xlsx")
+
+GSH.results <- results
+
+ggplot(results, )
