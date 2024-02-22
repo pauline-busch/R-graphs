6fdcc8ed2b16d074102141d2d6ae0e3e2507ab76
pauline-busch <miri@mccon.de>
2024-02-22 11:13:29 +0100

    finished analysis

--- Scripts/PIEZO patient.R
+++ Scripts/PIEZO patient.R
@@ -13,11 +13,19 @@ ATP.results <- results %>% filter(experiment == "GSH")
 GSH.graph <- ggplot(GSH.results, aes(sample, value, fill = sample)) + 
   
   geom_col() +
-  scale_fill_manual(values = c("#b00000", "#b19999"))
+  scale_fill_manual(values = c("#91d3ff", "#2870ed")) +
+  ylab("GSH [µmol / g Hb]") +
+  xlab("") +
+  theme(legend.position = "none") +
+  geom_text(aes(label = round(value, 1)), vjust = 1.5, colour = "white")
 
 ATP.graph <- ggplot(ATP.results, aes(sample, value, fill = sample)) + 
   
   geom_col() +
-  scale_fill_manual(values = c("#b00000", "#b19999"))
+  scale_fill_manual(values = c("#91d3ff", "#2870ed")) +
+  ylab("ATP [mol / g Hb]") +
+  xlab("") +
+  theme(legend.position = "none") +
+  geom_text(aes(label = round(value, 1)), vjust = 1.5, colour = "white")
 
-print(ATP.graph)
+print(ggarrange(GSH.graph, ATP.graph))
